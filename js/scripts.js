c5727c9f1ca01e78be5529ca58dbc9f13add208c
Golczi <pgol200014@gmail.com>
2014-02-24 21:09:13 +0100

    nowa struktura

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,60 @@
 $(document).ready(function () {
 
+
+	//config
 	var allQuestions = [{question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0},
-						{question: "Drugie pytanie", choices: ["Odpowiedz1", "Odpowiedz1", "Odpowiedz1", "Odpowiedz1"], correctAnswer:0}];
+						{question: "Drugie pytanie", choices: ["Odpowiedz1", "Odpowiedz1", "Odpowiedz1", "Odpowiedz1"], correctAnswer:0}],
+	questionIndex = 0,
+	indexMax = allQuestions.length,
+	correctIndex = allQuestions[0].correctAnswer;
+	question = allQuestions[0].question,
+	answers = allQuestions[0].choices,
+	$htmlQuestion = $('#question'),
+	$htmlAnswers = $('#answers'),
+	$nextButton = $('.next');
+	$prevButton = $('.prev');
 
-	var $questionhtml = $('#question');
-
-	var $answers = $('#answers');
-
-	for(var i in allQuestions) {
-		var $question = allQuestions[i].question;
-		console.log($question);
-		$questionhtml.html($question);
-		for(var j in allQuestions[i].choices) {
-			$answers.append('<li><input type="radio" name="answer" data-answer="' + j +'" value="' + allQuestions[i].choices[j] + '" class="answer">' + allQuestions[i].choices[j] + '</li>');
+	//methods
+	function appendQuestion() {
+		$htmlQuestion.html(question);
+		$htmlAnswers.html('');
+		for(var i in answers) {
+			$htmlAnswers.append('<li data-answer=' + i +'>' + answers[i] + '</li>');
 		}
-		var correct = allQuestions[i].correctAnswer;
-		$('.answer').on('click', function(){
-			if($(this).data('answer') === correct) {
-				console.log('dobra dop');
-			} else {
-				console.log('zla odp');
-			}
-		});
 	}
+	function nextQuestion() {
+		questionIndex += 1;
+		if(questionIndex < indexMax) {
+			question = allQuestions[questionIndex].question;
+			answers = allQuestions[questionIndex].choices;
+		} else {
+			alert('koniec pytan');
+		}
+	}
+	function prevQuestion() {
+		questionIndex -= 1;
+		if(questionIndex < indexMax) {
+			question = allQuestions[questionIndex].question;
+			answers = allQuestions[questionIndex].choices;
+		} else {
+			alert('nie mozna sie dalej cofnac');
+		}
+	}
+	function checkAnswer() {
+	}
+	function insertNextQuestion() {
+		nextQuestion();
+		appendQuestion();
+	}
+	function checkAnswer() {
+		if( correctIndex === $(this).data('answer') ) {
+			console.log('true');
+		} else {
+			console.log('false');
+		}
+	}
+	//main 
+	appendQuestion();
+	$nextButton.on('click', insertNextQuestion);
+	$htmlAnswers.on('click', 'li', checkAnswer);
 });
-//TO dop
-//obsluga wiekszej ilosci pytan
-//interaktywne menu po lewej
-//pare efektow z JQ
-
-
-// Quiz initiation
-  // beginning();
-  // generate_question();
-  // evaluate_answer();
-  // previous_question();
