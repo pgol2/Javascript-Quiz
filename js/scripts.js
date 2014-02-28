9a2fde24dc06a3a923384ade01af7d81faa50931
Golczi <pgol200014@gmail.com>
2014-02-28 23:17:29 +0100

    simpler, better

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,65 @@
 "use strict";
+
 $(document).ready(function () {
+
+
 //config
 var allQuestions = [{question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer: 0},
                     {question: "Drugie pytanie", choices: ["Odpowiedz1", "Odpowiedz1", "Odpowiedz1", "Odpowiedz1"], correctAnswer: 0},
                     {question: "Stolica Polski to? ", choices: ["Krakow", "Skrobow", "Warszawa", "Niestety Warszawa"], correctAnswer: 3}],
-questionIndex = 0,
-indexMax = allQuestions.length,
-correctIndex = allQuestions[0].correctAnswer,
-question = allQuestions[0].question,
-answers = allQuestions[0].choices,
-correct = 0,
+questionIndex = 0,                              //index aktualnego pytania 
+indexMax = allQuestions.length,                 //maksymalny index pytan 
+correctIndex = allQuestions[0].correctAnswer,   //index prawidlowej odp
+question = allQuestions[0].question,            //akutalna odpowiedz wyswietlana
+answers = allQuestions[0].choices,              //akutalne pytanie                        
 $htmlQuestion = $('#question'),
-$htmlAnswers = $('#answers'),
-$nextButton = $('.next'),
-$prevButton = $('.prev');
+$htmlAnswers = $('#answers');
 
-//methods
-function appendQuestion() {
-    $htmlQuestion.html(question);
-    $htmlAnswers.html('');
-    var list = "";
-    for(var i in answers) {
-        list += '<li data-answer=' + i +'>' + answers[i] + '</li>';
-    }
-    $htmlAnswers.html(list);
-}
-function nextQuestion() {
-    questionIndex += 1;
+
+function getNextQuestion() {
     if(questionIndex < indexMax) {
+        correctIndex = allQuestions[questionIndex].correctAnswer;
         question = allQuestions[questionIndex].question;
-        answers = allQuestions[questionIndex].choices;
+        //console.log("poprawna" + correctIndex);
+        $htmlQuestion.html(question);
 
-        if(questionIndex === indexMax-1) {
-            $nextButton.html('finish');
+        answers = allQuestions[questionIndex].choices;
+        var outAnswers = ""; 
+        for(var i in answers) {
+            outAnswers += '<li data-answer="' + i + '">' + answers[i] + "</li>"; 
         }
+        $htmlAnswers.html(outAnswers);
+        questionIndex++;
     } else {
         getScore();
     }
 }
-function insertNextQuestion() {
-    nextQuestion();
-    appendQuestion();
-}
-function checkAnswer() {
-    if( correctIndex === $(this).data('answer') ) {
-        ++correct;
-        console.log(correct);
-        alert('dobrze');
+
+function check() {
+    if($(this).data('answer') === correctIndex) {
+         $(".prompt").hide();
+        getNextQuestion();
     } else {
-        alert('zle');
+        promptUser("niestety zla odp", "red");
     }
 }
-function getScore() {
-    alert('twoj wynik to: ' + Math.round(correct/allQuestions.length * 100) + " %");
+function promptUser(message, color) {
+    var $message = $(".prompt");
+    $message.css({color: color});
+    $message.html(message);
+    $message.show();
 }
 
-//main 
-appendQuestion();
-$nextButton.on('click', insertNextQuestion);
-$htmlAnswers.on('click', 'li', checkAnswer);
+function getScore() {
+    promptUser("koniec na dzis - gratulacje !", "green");
+}
 
+getNextQuestion();
+$htmlAnswers.on('click', 'li', check);
 });
 
-// Quiz initiation
-// beginning();
-// generate_question();
-// evaluate_answer();
-// previous_question();
 
+// To do
+// -object
+// -bootstrap & handlebars
 
