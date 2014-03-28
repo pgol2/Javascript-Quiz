1fd9ab82e718a5d8387ef0b74b9ab1d43a2d3470
Golczi <pgol200014@gmail.com>
2014-03-28 15:46:02 +0100

    counting visits and remembering users - HTML5 storage

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,8 @@ Quiz {
     init(config),
     bindTemplates(),
 }*
+TODO:
+loading JSON file 
 */
 ///////////////////////
 
@@ -27,6 +29,8 @@ var Quiz = {
         this.bindEvents();
         this.config.container.append(this.htmlOut);
         this.startTime = new Date();
+        this.countVisits();
+        this.getUserName();
     },
     bindEvents: function() {
         this.config.container.on('click', 'li', this.checkAnswer);
@@ -38,17 +42,37 @@ var Quiz = {
             $('.container').html(this.htmlOut);
         } else {
             var endTime = new Date();
-            $('.container').html("koniec pytan twoj czas to: " + ( endTime- this.startTime)/1000);
+            $('.container').html("koniec pytan twoj czas to: " + ( endTime- this.startTime)/1000 );
         }
     },
     checkAnswer: function() { // this - czyli klikniety link 
         var that = Quiz;
-        if($(this).data('answer') == Quiz.config.allQuestions[Quiz.questionIndex].correctAnswer) {
+        if($(this).data('answer') === Quiz.config.allQuestions[Quiz.questionIndex].correctAnswer) {
             console.log('correct');
             that.getNextQuestion();
         } else {
             console.log('nope');
             alert('zła odpowiedź, popraw się!');
+        }
+    },
+    countVisits: function () {
+        if(localStorage.getItem('visits') === null) { //periwsza wizyta
+            localStorage.setItem('visits', 1);
+            console.log(localStorage.getItem('visits'));
+        } else {
+            var value = parseInt(localStorage.getItem('visits'));
+            value += 1;
+            localStorage.setItem('visits', value);
+            console.log(localStorage.getItem('visits'));
+        }
+    },
+    getUserName: function() {
+        var name = "";
+        if(localStorage.getItem('username') === null) {
+            name = prompt("What's your name?  ", "type here ");
+            localStorage.setItem('username', name);
+        } else {
+            alert("hello " + localStorage.getItem('username') );
         }
     }
 
@@ -56,22 +80,26 @@ var Quiz = {
 
 var allQuestions = [        //objekt ze wszystkimi pytaniami
 {
-    question: "Who is Prime Minister of the United Kingdom?", 
-    choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+    question: "Complete this phrase. As sick as a...", 
+    choices: ["Penguin", "Parrot", "Puffin", "Partridge"], 
     correctAnswer: 0
 },
-{question: "Drugie pytanie", 
-choices: ["Odpowiedz1", "Odpowiedz1", "Odpowiedz1", "Odpowiedz1"], 
-correctAnswer: 0
+{
+    question: "Which legal document states a person's wishes regarding the disposal of their property after death?", 
+    choices: ["Will", "Shall", "Would", "Should"], 
+    correctAnswer: 0
 },
-{question: "Stolica Polski to? ", 
-choices: ["Krakow", "Skrobow", "Warszawa", "Niestety Warszawa"], 
-correctAnswer: 3
+{
+    question: "Complete the title of the James Bond film The Man With The Golden...", 
+    choices: ["Eagle", "Tooth", "Delicious", "Gun" ], 
+    correctAnswer: 3
+},
+{
+    question: " In which sport do two teams pull at the opposite ends of a rope?", 
+    choices: ["Ice hockey", "Basketball", "Tug of war", "Polo"], 
+    correctAnswer: 2
 }
 ];
-
-
-
 
 $(document).ready(function() {
     Quiz.init({
@@ -81,4 +109,6 @@ $(document).ready(function() {
     });
 
 });
+
+
 
